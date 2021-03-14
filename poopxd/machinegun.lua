7871f95b1483da19ebc119ce27f787ac571b470b
iNeedABetterUser <79945364+iNeedABetterUser@users.noreply.github.com>
2021-03-14 15:45:14 -0400

    Create machinegun.lua

---
+++ poopxd/machinegun.lua
@@ -0,0 +1,13 @@
+-- A script that automatically reloads your musket
+local name = "Musket" -- Change this to the Musket tool's name
+
+game:GetService("RunService").RenderStepped:Connect(function()
+    local musket = game.Players.LocalPlayer.Character:FindFirstChild(name)
+    if musket then
+        musket.Loaded.Value = true
+        musket.Ammo.Value = 1
+        musket.MadeReady.Value = true
+        musket.LDS.Value = true
+        musket.Aiming.Value = true
+    end
+end)
