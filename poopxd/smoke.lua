031cd908223fad54a07ad99fd18b16ede0b96c46
iNeedABetterUser <79945364+iNeedABetterUser@users.noreply.github.com>
2021-03-15 18:12:21 -0400

    Update smoke.lua

--- poopxd/smoke.lua
+++ poopxd/smoke.lua
@@ -3,12 +3,12 @@ local name = "Musket" -- Change this to the Musket tool's name
 
 local Players = game:GetService("Players")
 while true do
-    for i,v in pairs(Players:GetPlayers()) do
+	for i,v in pairs(Players:GetPlayers()) do
 		if v ~= nil and v.Character ~= nil then
-        	if v.Character:FindFirstChild(name) then
-            	v.Character.Musket.Core.Fire:FireServer()
-        	end
-        end
-    end
-    wait(0.1)
+			if v.Character:FindFirstChild(name) then
+				v.Character.Musket.Core.Fire:FireServer()
+			end
+		end
+	end
+	wait(0.1)
 end
