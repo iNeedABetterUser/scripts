937b432035a423151e256a7f27d5789cc9e9d071
iNeedABetterUser <79945364+iNeedABetterUser@users.noreply.github.com>
2021-03-17 19:35:31 -0400

    Update kill_all.lua

--- poopxd/kill_all.lua
+++ poopxd/kill_all.lua
@@ -1,11 +1,10 @@
 -- Simple script that kills everyone in the server
+local name = "Musket" -- Change this to the Musket tool's name
 
 -- THIS SCRIPT REQUIRES keyfinder.lua IN THE AUTOEXECUTE FOLDER
 -- The keyfinder.lua script is here: https://raw.githubusercontent.com/iNeedABetterUser/scripts/main/keyfinder.lua
 
-local Players = game:GetService("Players")
-local me = Players.LocalPlayer
 
-for i,v in pairs(Players:GetPlayers()) do
-    game:GetService("ReplicatedStorage").ServerEvents.Damage:FireServer(_G.HistoricalKey, me.Character.Musket.Animation.BulletDamage, v.Character["Head"], me)
+for i,v in pairs(game:GetService("Players"):GetPlayers()) do
+    game:GetService("ReplicatedStorage").ServerEvents.Damage:FireServer(_G.HistoricalKey, game:GetService("Players").LocalPlayer.Character[name].Animation.BulletDamage, v.Character["Head"], me)
 end
