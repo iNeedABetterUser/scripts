ac4a16f5bbd0bb3d2fc775e1bcbf85059ab97785
iNeedABetterUser <79945364+iNeedABetterUser@users.noreply.github.com>
2021-03-18 19:38:15 -0400

    Update kill_all.lua

--- poopxd/kill_all.lua
+++ poopxd/kill_all.lua
@@ -6,6 +6,6 @@ local name = "Musket" -- Change this to the Musket tool's name
 
 for i,v in pairs(game:GetService("Players"):GetPlayers()) do
     if v ~= nil and v.Character ~= nil then
-        game:GetService("ReplicatedStorage").ServerEvents.Damage:FireServer(_G.HistoricalKey, game:GetService("Players").LocalPlayer.Character[name].Animation.BulletDamage, v.Character["Head"], me)
+        game:GetService("ReplicatedStorage").ServerEvents.Damage:FireServer(_G.HistoricalKey, game:GetService("Players").LocalPlayer.Character[name].Animation.BulletDamage, v.Character["Head"], game:GetService("Players").LocalPlayer)
     end
 end
