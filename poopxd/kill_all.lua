019e15f57fbb5acdeae13bb7459b14f72f96c0a7
iNeedABetterUser <79945364+iNeedABetterUser@users.noreply.github.com>
2021-03-18 19:58:56 -0400

    Update kill_all.lua

--- poopxd/kill_all.lua
+++ poopxd/kill_all.lua
@@ -4,8 +4,8 @@ local name = "Musket" -- Change this to the Musket tool's name
 -- THIS SCRIPT REQUIRES keyfinder.lua IN THE AUTOEXECUTE FOLDER
 -- The keyfinder.lua script is here: https://raw.githubusercontent.com/iNeedABetterUser/scripts/main/keyfinder.lua
 
-for i,v in pairs(game:GetService("Players"):GetPlayers()) do
-    if v ~= nil and v.Character ~= nil then
+for i,plr in pairs(game:GetService("Players"):GetPlayers()) do
+    if plr ~= nil and plr.Character ~= nil then
         game:GetService("ReplicatedStorage").ServerEvents.Damage:FireServer(_G.HistoricalKey, game:GetService("Players").LocalPlayer.Character[name].Animation.BulletDamage, v.Character["Head"], game:GetService("Players").LocalPlayer)
     end
 end
