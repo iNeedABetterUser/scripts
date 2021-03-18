dbf8075eb350041f00dbde4ac13c2f79455d9879
iNeedABetterUser <79945364+iNeedABetterUser@users.noreply.github.com>
2021-03-18 19:58:25 -0400

    Update smoke.lua

--- poopxd/smoke.lua
+++ poopxd/smoke.lua
@@ -1,9 +1,8 @@
 -- Simple script that spams smoke out of every player's musket
 local name = "Musket" -- Change this to the Musket tool's name
 
-local Players = game:GetService("Players")
 while true do
-	for i,plr in pairs(Players:GetPlayers()) do
+	for i,plr in pairs(game:GetService("Players"):GetPlayers()) do
 		if plr ~= nil and plr.Character ~= nil then
 			if plr.Character:FindFirstChild(name) then
 				plr.Character[name].Core.Fire:FireServer()
