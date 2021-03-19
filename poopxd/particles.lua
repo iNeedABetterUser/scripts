c8016eea1c76949634168110d1ff4884e3a5dcd4
iNeedABetterUser <79945364+iNeedABetterUser@users.noreply.github.com>
2021-03-18 20:00:15 -0400

    Update particles.lua

--- poopxd/particles.lua
+++ poopxd/particles.lua
@@ -1,10 +1,9 @@
 -- Simple script that creates blood particles around every player's head
-
-local Players = game:GetService("Players")
+ 
 while true do
-    for i,v in pairs(Players:GetPlayers()) do
-        if v ~= nil and v.Character ~= nil then
-            game:GetService("ReplicatedStorage").ServerEvents.Projectile:FireServer(v.Character.Torso, v.Character.HumanoidRootPart.Position)
+    for i,plr in pairs(game:GetService("Players"):GetPlayers()) do
+        if plr ~= nil and plr.Character ~= nil then
+            game:GetService("ReplicatedStorage").ServerEvents.Projectile:FireServer(plr.Character.Torso, plr.Character.HumanoidRootPart.Position)
         end
     end
     wait(0.1)
