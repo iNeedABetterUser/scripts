6ab1915a8202c78f9debf823cae233ebc57468c2
iNeedABetterUser <79945364+iNeedABetterUser@users.noreply.github.com>
2021-03-18 20:02:18 -0400

    Update remove_ammo.lua

--- frontier/remove_ammo.lua
+++ frontier/remove_ammo.lua
@@ -1,14 +1,14 @@
 -- Simple script that breaks the Frontier musket by removing everyone's ammo
+local name = "Musket" -- Change this to the Musket tool's name
 
 -- THIS SCRIPT REQUIRES keyfinder.lua IN THE AUTOEXECUTE FOLDER
 -- The keyfinder.lua script is here: https://raw.githubusercontent.com/iNeedABetterUser/scripts/main/keyfinder.lua
 
 while true do
-    local Players = game:GetService("Players")
-    for i,v in pairs(Players:GetPlayers()) do
-		if v ~= nil and v.Character ~= nil then
-        	if v.Character:FindFirstChild("Musket") then
-                game:GetService("ReplicatedStorage").gunCollection.Events.ChangeVal:FireServer(_G.FrontierKey, v.Character.Musket._info.Ammo, 0)
+    for i,plr in pairs(game:GetService("Players"):GetPlayers()) do
+		if plr ~= nil and plr.Character ~= nil then
+        	if plr.Character:FindFirstChild(name) then
+                game:GetService("ReplicatedStorage").gunCollection.Events.ChangeVal:FireServer(_G.FrontierKey, plr.Character[name]._info.Ammo, 0)
         	end
         end
     end
