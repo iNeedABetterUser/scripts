5d8453543c4d0b64f9285510543b160e7f9462ff
iNeedABetterUser <79945364+iNeedABetterUser@users.noreply.github.com>
2021-03-18 20:00:41 -0400

    Update remove_ammo.lua

--- poopxd/remove_ammo.lua
+++ poopxd/remove_ammo.lua
@@ -5,10 +5,10 @@ local name = "Musket" -- Change this to the Musket tool's name
 -- The keyfinder.lua script is here: https://raw.githubusercontent.com/iNeedABetterUser/scripts/main/keyfinder.lua
 
 while true do
-    for i,v in pairs(game:GetService("Players"):GetPlayers()) do
-        if v ~= nil and v.Character ~= nil then
-            if v.Character:FindFirstChild(name) then
-                game:GetService("ReplicatedStorage").ServerEvents.ChangeVal:FireServer(_G.HistoricalKey, v.Character[name].Ammo, 0)
+    for i,plr in pairs(game:GetService("Players"):GetPlayers()) do
+        if plr ~= nil and plr.Character ~= nil then
+            if plr.Character:FindFirstChild(name) then
+                game:GetService("ReplicatedStorage").ServerEvents.ChangeVal:FireServer(_G.HistoricalKey, plr.Character[name].Ammo, 0)
             end
         end
     end
