8e04c0263d647a215191fb7286c8185e21e8aa5d
iNeedABetterUser <79945364+iNeedABetterUser@users.noreply.github.com>
2021-03-20 23:57:41 -0400

    Update morale_boost.lua

--- europainvicta/morale_boost.lua
+++ europainvicta/morale_boost.lua
@@ -1,4 +1,4 @@
--- Simple script that boosts your morale to 100 points
+-- Simple script that permanently boosts your morale to 100 points
 
 game:GetService("RunService").RenderStepped:Connect(function()
     game:GetService("Players").LocalPlayer.Settings.Morale.Value = 100
